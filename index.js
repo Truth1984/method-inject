74a59a37b81c6519cc7872bf95ec48582e1a653d
awada <zhengyao365@163.com>
2021-01-13 12:08:56 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * @param {?(name:string, args:?array)=>{}} preAction
+ * @param {?(name:string, result)=>{}} postAction
+ */
+module.exports = (target, preAction = () => {}, postAction = () => {}) => {
+  let asyncfunc = preAction instanceof Function && preAction.constructor.name === "AsyncFunction";
+  let modify = (i) => {
+    if (target[i] instanceof Function) {
+      let targetFunc = target[i];
+      if (asyncfunc) {
+        target[i] = async (...args) => {
+          await preAction(i, args);
+          let result = targetFunc(...args);
+          await postAction(i, result);
+          return result;
+        };
+      } else {
+        target[i] = (...args) => {
+          preAction(i, args);
+          let result = targetFunc(...args);
+          postAction(i, result);
+          return result;
+        };
+      }
+    }
+  };
+
+  if (typeof target === "object" && target.constructor.name != "Object") {
+    //class
+    let properties = Object.getOwnPropertyNames(Object.getPrototypeOf(target));
+    for (let i of properties) modify(i);
+  } else {
+    //obj
+    for (let i of Object.keys(target)) modify(i);
+  }
+  return target;
+};
---
+++ readme.md
@@ -0,0 +1,42 @@
+## func-injector
+
+add custom function before and after each function call
+
+usage: you don't want to modify an old module, but you want to know what each function produces and `proxy` doesn't serve your purpose
+
+## example
+
+```js
+const MI = require("func-injector");
+const CLS = class {
+  constructor() {}
+  add(a, b) {
+    return a + b;
+  }
+};
+
+let map = {
+  a: 10,
+  add: (a, b) => a + b,
+};
+let cls = new CLS();
+
+let logger = (name, value) => console.log({ name, value });
+
+MI(map, logger, logger);
+MI(cls, logger, logger);
+
+map.add(1, 3);
+//{ name: 'add', value: [ 1, 3 ] }
+//{ name: 'add', value: 4 }
+```
+
+## api
+
+#### (target, preAction = () => {}, postAction = () => {}) => {}
+
+preAction: (name, args) action before performing task,
+
+postAction: (name, value) action after getting a value
+
+all of them can be async, but the result will be async too, so be careful
